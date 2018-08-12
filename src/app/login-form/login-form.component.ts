129a585138d727cf8ce8bbcdde424aa45bf8adae
ray1295 <ralake95@gmail.com>
2018-08-12 12:56:18 +0100

    SCSS issue

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  templateUrl: './login-form.component.html'
+  // styleUrls: ['./login-form.component.scss']
 })
+
 export class LoginFormComponent implements OnInit {
+
+  constructor(private builder: FormBuilder) {
+  }
   username = new FormControl('', [
     Validators.required,
     Validators.minLength(5)
   ]);
 
-  password = new FormControl('', [Validators.required]);
+  password = new FormControl('', [
+    Validators.required,
+  ]);
 
   loginForm: FormGroup = this.builder.group({
     username: this.username,
     password: this.password
   });
-  constructor(private builder: FormBuilder) { }
-    login () {
-      console.log(this.loginForm.value);
-    }
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+    // Attempt logging in...
+  }
+
+   hasExclamationMark(input: FormControl) {
+    const hasExclamation = input.value.indexOf('!') >= 0;
+    return hasExclamation ? null : { needsExclamation: true };
   }
 
 }
